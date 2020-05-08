f6b897774d9fc9e5532e9d036628375b5a89730f
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-08 12:48:18 -0500

    tarea15 v0.0.0.5

--- tareas/tarea15/index.php
+++ tareas/tarea15/index.php
@@ -14,8 +14,13 @@
             <br>
             <h1>Vence a Gon en su propio juego!</h1>
             <br>
+            <div align="center">
+                <h1>Escoje tu siguiente movimiento!</h1>
+                <img src="images/victory.png" alt="Piedra" class="imagen" style="width: 100%">
+                <br>
+            </div>
             <div class="container">
-                <img src="images/roca.png" alt="Piedra" class="imagen" style="width: 100%">
+                <img src="images/challenge.gif" alt="Piedra" class="imagen" style="width: 100%">
                 <div class="middle">
                     <button class="btn">¡PIEDRA!</button>
                 </div>

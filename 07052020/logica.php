d381d6f1a7a1bf5f6be6f8780b44a3f2f18ddfe9
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-07 20:17:18 -0500

    Trabajo 07052020

---
+++ 07052020/logica.php
@@ -0,0 +1,13 @@
+<?php
+$dato1 = $_GET["num1"];
+$dato2 = $_GET["num2"];
+$dato3 = $_GET["num3"];
+$resultado = $dato1+$dato2+$dato3;
+
+echo"Resultado es: ".$resultado;
+
+echo"
+<table align= 'center'>
+    <tr><td style= 'border: 2px solid black; background-color:red; ' > <h1> El resultado es: ".$resultado." </h1></td></tr>
+    </table>
+?>"
---
+++ 07052020/numero.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="https://github.com/Adrian-ICO/Garcia_Chavez/tree/master/tareas/tarea14/css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+</head>
+
+<body>
+    <form action="logica.php"></form>
+    <br>
+    Ingrese el primer numero: <input type="num1"><br>
+    Ingrese el segundo numero: <input type="num2"><br>
+    Ingrese el tercer numero: <input type="num3"><br>
+
+</body>
+
+</html>

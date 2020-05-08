2c4f4835b0d91bb24f769390eceafac0bdbf0262
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-08 12:51:06 -0500

    tarea15 0.0.0.7

--- tareas/tarea15/index.php
+++ tareas/tarea15/index.php
@@ -16,11 +16,11 @@
             <br>
             <div class="imagen" align="center">
                 <h1>Escoje tu siguiente movimiento!</h1>
-                <img src="images/victory.png" alt="Piedra" class="imagen" style="width: 100%">
+                <img src="images/challenge.gif" alt="Piedra" class="imagen" style="width: 100%">
                 <br>
             </div>
             <div class="container">
-                <img src="images/challenge.gif" alt="Piedra" class="imagen" style="width: 100%">
+                <img src="images/roca.png" alt="Piedra" class="imagen" style="width: 100%">
                 <div class="middle">
                     <button class="btn">¡PIEDRA!</button>
                 </div>

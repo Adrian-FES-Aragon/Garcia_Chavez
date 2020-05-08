aa8d8391dc7dcb9e4a6a6efaba3938c9e2404dda
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-08 12:40:12 -0500

    tarea15 v0.0.0.1

--- tareas/tarea15/style.php
+++ tareas/tarea15/style.php
@@ -1,3 +1,22 @@
+<?php header("Content-type: text/css");
+
+//Creo un array con varios colores
+$color[0]='#FF5733';
+$color[1]='#33E0FF';
+$color[2]='#8D33FF';
+$color[3]='#FFCA33';
+$color[4]='#90FF33';
+$color[5]='#335BFF';
+
+// Elijo un color aleatorio
+$i=rand(0,5);
+$u=rand(5,0);
+?>
+
+/* indico donde quiero que aparezca el color aleatorio */
+body { 
+    background-color: <?=$color[$i]?>;
+}
 .container{
     position: relative;
     width: 50%;

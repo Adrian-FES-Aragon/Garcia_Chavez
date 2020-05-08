f017eef528ce5fa89e7b96a5a96b48902365b0d0
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-07 21:48:33 -0500

    1

--- 07052020/logica.php
+++ 07052020/logica.php
@@ -7,7 +7,7 @@ $dato3 = $_GET["num3"];
 $resultado = $dato1+$dato2+$dato3;
 
 echo"
-<table class="table1">
+<table class='table1'>
     <tr>
          <td  ' > 
             <h1> El resultado es: ".$resultado." </h1>

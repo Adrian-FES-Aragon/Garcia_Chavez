ff92e765c74b77d7b61349d5ea20d21e5278c5c8
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-08 12:36:04 -0500

    tarea15 v.0.0.0.0

---
+++ tareas/tarea15/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" type="text/css" href="style.php">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yan Ken Po</title>
+</head>
+
+<body>
+    <div>
+        <form action="logic.php">
+            <br>
+            <h1>Vence a Gon en su propio juego!</h1>
+            <br>
+            <div class="container">
+                <img src="images/roca.png" alt="Piedra" class="imagen" style="width: 100%">
+                <div class="middle">
+                    <button class="btn">¡PIEDRA!</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>

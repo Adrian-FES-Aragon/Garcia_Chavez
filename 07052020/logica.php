069555959fc5f44b6bff4a1b27daa338d11afe0f
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-07 21:37:34 -0500

    test 1.0

--- 07052020/logica.php
+++ 07052020/logica.php
@@ -1,4 +1,4 @@
-<?php
+<?php header("Content-type: text/css"); 
 
 $dato1 = $_GET["num1"];
 $dato2 = $_GET["num2"];
@@ -7,12 +7,16 @@ $dato3 = $_GET["num3"];
 $resultado = $dato1+$dato2+$dato3;
 
 echo"
+<head>
+<link rel="stylesheet" type="text/css" href="estilos.php">
+</head>
+<body>
 <table align='center'>
     <tr>
          <td style='border: 2px solid black; background-color:red; ' > 
             <h1> El resultado es: ".$resultado." </h1>
         </td>
     </tr>
-</table>";
-echo "<body>";
+</table>
+</body>;"
 ?>

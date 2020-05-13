a36f9fdd24d32c8571b8d6140bd38efe88001fac
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-12 20:58:56 -0500

    bd

---
+++ 12052020/fesaragon.sql
@@ -0,0 +1,23 @@
+DROP SCHEMA IF EXISTS `fes_aragon`;
+CREATE SCHEMA IF NOT EXISTS `fes_aragon` DEFAULT CHARACTER SET utf8 collate utf8_spanish2_ci;
+USE `fes_aragon`;
+
+CREATE TABLE `ALUMNO`
+(
+    `user_name` TEXT NOT NULL,
+    `career` TEXT NOT NULL,
+    `code` INT(10) NOT NULL,
+    `address` TEXT NOT NULL,
+    `number` INT(10) NOT NULL,
+    `email` TEXT NOT NULL,
+    `password` VARCHAR(8) NOT NULL,
+    `registration_date` DATETIME NOT NULL,
+    `permissions` INT (11) NOT NULL DEFAULT '2'
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+INSERT INTO `ALUMNO`(`user_name`, `career`, `code`, `address`, `email`, `password`, `registration_date`, `permissions`) 
+VALUES (`Pepe`, `ico`, `123456`, `tijuana ajua12`, `pussydestroyerx99x@hotmail.com`,`123456x`, `2020-05-12 14:11:00`, 2);
+
+ALTER TABLE `ALUMNO`
+    ADD PRIMARY KEY(`code`);
+COMMIT;

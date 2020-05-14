907f9879a413f74966a09c88b9af556761dfd902
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-14 01:08:00 -0500

    logic 2.0

--- tareas/tarea15/logic.php
+++ tareas/tarea15/logic.php
@@ -1,18 +1,28 @@
 <?php
 
-$choice[0] = $_POST["evento_piedra"];
-$choice[1] = $_POST["evento_papel"];
-$choice[2] = $_POST["evento_tijeras"];
+$rock = $_POST['evento_piedra'];
+$paper = $_POST['evento_papel'];
+$knife = $_POST['evento_tijeras'];
 
+$opt[0] = $rock;
+$opt[1] = $paper;
+$opt[2] = $knife;
 $i = rand(0, 2);
+echo "i: " . $i;
+echo "opt: " . $opt;
 
-if ($i == 0) {
-    echo "Gon ha elegido ¡PAPEL! tu pierdes";
-    header("index.php");  # code...
-} else if ($i== 1) {
-    echo "Gon ha elegido ¡TIJERAS! tu pierdes";
+if ($i == $opt) {
+    echo "Gon ha elegido lo mismo! es un empate, considerate afortunado";
     header("index.php");
-} else if ($i == 2) {
+} else if ($i == 0 && $opt == 2) {
     echo "Gon ha elegido ¡PIEDRA! tu pierdes";
     header("index.php");
+} else if ($i == 1 && $opt == 0) {
+    echo "Gon ha elegido ¡PAPEL! tu pierdes";
+    header("index.php");
+} else if ($i == 2 && $opt == 1) {
+    echo "Gon ha elegido ¡TIJERAS! tu pierdes";
+    header("index.php");
+} else {
+    echo "¡FELICIDADES HAS GANADO!";
 }

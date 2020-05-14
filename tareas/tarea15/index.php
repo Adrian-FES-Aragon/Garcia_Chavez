9478c25f75f812168d33977d5d2ed048a8ad0915
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-14 00:35:15 -0500

    logi

--- tareas/tarea15/index.php
+++ tareas/tarea15/index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+    <link rel="Shortcut icon" href="images/tareaTerminada.png">
     <link rel="stylesheet" type="text/css" href="style.php">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,7 +11,7 @@
 
 <body>
     <div align="center">
-        <form action="logic.php">
+        <form action="logic.php" name="form1" id="form1" method="POST">
             <br>
             <h1>Vence a Gon en su propio juego!</h1>
             <br>
@@ -27,7 +28,7 @@
                         <div class="container">
                             <img src="images/roca.png" alt="Piedra" class="imagen" style="width: 100%">
                             <div class="middle">
-                                <button class="btn">¡PIEDRA!</button>
+                                <input type="submit" class="btn" value="¡PIEDRA!" id="evento_piedra" name="eveto_piedra"/>
                             </div>
                         </div>
                     </td>
@@ -36,7 +37,7 @@
                         <div class="container">
                             <img src="images/papel.png" alt="Papel" class="imagen" >
                             <div class="middle">
-                                <button class="btn">¡PAPEL!</button>
+                                <input type="submit" class="btn" value="¡PAPEL!" id="evento_papel" name="evento_papel"/>
                             </div>
                         </div>
                     </td>
@@ -45,11 +46,10 @@
                         <div class="container">
                             <img src="images/tijeras.jpg" alt="Tijeras" class="imagen" style="width: 100%">
                             <div class="middle">
-                                <button class="btn">¡TIJERAS!</button>
+                                <input type="submit" class="btn" value="¡TIJERAS!" id="evento_tijeras" name="evento_tijeras" />
                             </div>
                         </div>
                     </td>
-
                 </tr>
             </table>
         </form>
---
+++ tareas/tarea15/logic.php
@@ -0,0 +1,18 @@
+<?php
+
+$choice[0] = $_POST['evento_piedra'];
+$choice[1] = $_POST['evento_papel'];
+$choice[2] = $_POST['evento_tijeras'];
+
+$i = rand(0, 2);
+
+if ($i == 0) {
+    echo "Gon ha elegido ¡PAPEL! tu pierdes";
+    header("index.php");  # code...
+} else if ($i== 1) {
+    echo "Gon ha elegido ¡TIJERAS! tu pierdes";
+    header("index.php");
+} else if ($i == 2) {
+    echo "Gon ha elegido ¡PIEDRA! tu pierdes";
+    header("index.php");
+}

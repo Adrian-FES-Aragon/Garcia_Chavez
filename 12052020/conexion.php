3d8da70a1d36bb0c9ac6816387783c07be97d868
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-14 21:14:32 -0500

    .

--- 12052020/conexion.php
+++ 12052020/conexion.php
@@ -4,7 +4,7 @@ $user_db="root";
 $pass_db="";
 $name_db="test_php";
 
-$conexion= mysqli_connect($host_db, $user_db, $pass_db, $name_db);
+$conexion= mysqli_connect($host_db, $user_db, $pass_db);
 if ($conexion) {
     echo "<h1> MySql le esta dando permisos para ejecutar consultas</h1>";
 }else{

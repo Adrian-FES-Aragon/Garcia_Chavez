5b89c81c0dcd54e7bed21a94fbc55714f60ebce5
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-14 23:29:26 -0500

    prefinal

--- tareas/tarea15/logic.php
+++ tareas/tarea15/logic.php
@@ -5,7 +5,7 @@ if (isset($_POST['evento_piedra'])) {
     $evento_piedra = $_POST['evento_piedra'];
     echo "TU ELEGISTE: " . $evento_piedra . '<br>' . "Y GON: ";
 
-    $power = array("PAPEL", $evento_piedra, "TIJERAS");
+    $power = array("!PAPEL¡", $evento_piedra, "!TIJERAS¡");
     $rand = $power[array_rand($power)];
     echo $rand;
 
@@ -14,15 +14,31 @@ if (isset($_POST['evento_piedra'])) {
     }
 } else if (isset($_POST['evento_papel'])) {
     $evento_papel = $_POST['evento_papel'];
-    echo "TU ELEGISTE" . $evento_papel;
+    echo "TU ELEGISTE: " . $evento_papel . '<br>' . "Y GON: ";
+
+    $power = array("$evento_papel", "¡PIEDRA!", "¡TIJERAS!");
+    $rand = $power[array_rand($power)];
+    echo $rand;
+
+    if ($evento_papel == $rand) {
+        echo "" . '<br>' . "ES UN EMPATE!";
+    }
 }
-
-
-
 if (isset($_POST['evento_tijeras'])) {
     $evento_tijeras = $_POST['evento_tijeras'];
-    echo "Eleccion: " . $evento_tijeras;
+    echo "TU ELEGISTE: " . $evento_tijeras . '<br>' . "Y GON: ";
+
+    $power = array("!PAPEL¡", "!PIEDRA¡", $evento_tijeras);
+    $rand = $power[array_rand($power)];
+    echo $rand;
+
+    if ($evento_tijeras == $rand) {
+        echo "" . '<br>' . "ES UN EMPATE!";
+    }
 }
+
+
+
 
 
 /*

10b24e7e5c039e8332d7650ecd7c47c0ab8f360c
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-14 22:10:27 -0500

    5

--- tareas/tarea15/logic.php
+++ tareas/tarea15/logic.php
@@ -23,7 +23,7 @@ echo $input[$rand_keys[2]] . "<br>";
 
 $a=array("red","green","blue","yellow","brown");
 $random_keys=array_rand($a,3);
-echo $a[$random_keys[0]]."<br>";
+echo $a[$random_keys[5]]."<br>";
 
 
 

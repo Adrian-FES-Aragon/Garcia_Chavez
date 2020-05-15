00bba3456767be02f28ce06cbde50d92ce124b1f
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-14 21:59:40 -0500

    2

--- tareas/tarea15/logic.php
+++ tareas/tarea15/logic.php
@@ -15,8 +15,8 @@ if(isset($_POST['evento_tijeras'])) {
 }
 
 $input = array('evento_piedra', 'evento_papel','evento_tijeras');
-$rand_keys = array_rand($input, 1);
-echo $input[$rand_keys[1]] . "\n";
+$rand_keys = array_rand($input, 2);
+echo $input[$rand_keys[0]] . "\n";
 /*
 $input = array("Neo", "Morpheus", "Trinity", "Cypher", "Tank");
 $rand_keys = array_rand($input, 2);

2eb35866c29fb78b1419ef4cda28e9c77bd803b1
Adrian-ICO <adrian.ga_2009@hotmail.com>
2020-05-22 00:54:00 -0500

    correction

--- 19052020/mandar_registro.php
+++ 19052020/mandar_registro.php
@@ -1,39 +1,46 @@
 <?php
-$host_db="127.0.0.1";
-$user_db="root";
-$pass_db="";
-$name_db="fes_aragon";
-//conexion a la DB
-$conexion= new mysqli($host_db, $user_db, $pass_db, $name_db);
-mysqli_set_charset($conexion, 'utf8');
-//validacion si el usuario ya esta registrado
-$buscarUsuario="SELECT * FROM ALUMNO WHERE code = $_POST[cuenta]";
-//mandar al query
-$result=$conexion->query($buscarUsuario);
-//saber si tenemos un resultado y se almacena en una variable
-$count=mysqli_num_rows($result);
-//se hace la validacion del resultado para saber si esta registrado 
-if ($count == 1 ) { //se se encentra algun reegistro
-    echo"<h1>'El usuario con el numero de cuenta:' ".$_POST['cuenta']. "'Ya esta registrado'</h1>";
-}else{//se registra al usuario
-    //query para mandar el registro
-    mysqli_query($conexion, "INSERT INTO ALUMNO (
-    user_name,
-    career, 
-    code,
-    address,
-    number,
-    email,
+//conexion a la base datos
+include "conexion.php";
+mysqli_set_charset($conexion,'utf8');
+
+//declaracion de varibales para formulario
+
+$form_pass =  $_POST ['password'];
+
+$buscarUsuario = "SELECT * FROM usuarios where nombre_usuario = '$_POST[nombre_usuario]'";
+
+$result = $conexion -> query($buscarUsuario);
+$count = mysqli_num_rows($result);
+
+if($count ==1 ){
+    echo'El nombre se usuario ya a sido ocupado';
+    echo "<a href='registro.php'>Hacer Registro</a>";
+    
+}else{
+    mysqli_query($conexion, "INSERT INTO usuarios (
+    nombre,
+    direccion,
+    telefono,
+    correo,
+    nombre_usuario,
     password)
-    VALUES(
+        VALUES(
+    '$_POST[nombre]',
+    '$_POST[direccion]',
+    '$_POST[telefono]',
+    '$_POST[correo]',
     '$_POST[nombre_usuario]',
-    '$_POST[carrera]',
-    '$_POST[cuenta]',
-    '$_POST[nombre_completo]',
-    '$_POST[numero_celular]',
-    '$_POST[email]',
-    '$_POST[password]')");
-    echo"<h1> Usuario registrado exitosamente</h1>";
-    echo"<h2>Bienvenido: ".$_POST['nombre_usuario']. "</h2>";
+    '$_POST[password]'        
+        
+        
+    )");
+echo "<br />" . "<h2>" . "Usuario Creado Exitosamente!" . "</h2>";
+echo "<h4>" . "Bienvenido: " . $_POST['nombre'] . "</h4>" . "\n\n";
+echo "<h5>" . "<a href='./registro.php'>Iniciar Sesión</a>" . "</h5>";
+
+//termina el else
 }
+
+
+
 ?>
--- 19052020/registro.php
+++ 19052020/registro.php
@@ -1,49 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
+ <title>Registrar Usuario</title>
+ <meta charset = "utf-8">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro</title>
-</head>
-
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ </head>
+<script>
+    function valida(e){
+        tecla = (document.all) ? e.keyCode : e.which;
+        //Tecla de retroceso para borrar, siempre la permite
+        if (tecla==8){
+            return true;
+        }
+        //patron de entrada, en este caso solo acepta numero
+        patron =/[0-9]/;
+        tecla_final = String.fromCharCode(tecla);
+        return patron.test(tecla_final);
+    }
+</script>
 <body>
-    <header>
-        <h1 style="text-align:center">Registro alumnos ICO</h1>
-    </header>
-    <form action="mandar_registro.php" method="POST">
-
-    <label for="nombre_usuario"> Nombre de usuario: </label>
-    <input type="text" name="nombre_usuario" maxlength="155" require>
-    <br><br>
-
-    <label for="carrera">Carrera: </label>
-    <input type="text" name="carrera" maxlength="155" require>
-    <br><br>
-
-    <label for="cuenta">Numero de Cuenta: </label>
-    <input type="text" name="cuenta" maxlength="155" require>
-    <br><br>
-
-    <label for="nombre_completo"> Nombre completo: </label>
-    <input type="text" name="nombre_completo" maxlength="155" require>
-    <br><br>
-
-    <label for="numero_celular"> Numero de celular: </label>
-    <input type="text" name="numero_celular" maxlength="155" require>
-    <br><br>
-
-    <label for="email"> Correo electronico: </label>
-    <input type="email" name="email" maxlength="155" require>
-    <br><br>
-
-    <label for="password"> Ingresa tu contraseña: </label>
-    <input type="password" name="password" maxlength="155" require>
-    <br><br>
-
-    <input type="submit" name="submit" value="Registro de alumno">
-    <input type="reset" name="clear" value="Borrar campos">
-    </form>
-</body>
-
+ <header>
+ <h2 style="text-align:center">Registro Alumnos 2.0 </h2>
+  </header>
+<form style="text-align:center"  action="mandar_registro.php" method="post">
+ <hr />
+ <!--Nombre-->
+ 
+ <label for="nombre">Nombre Completo:</label><br>
+ <input type="text" name="nombre" maxlength="255"  required>
+ <br/><br/>
+ <!-- Direcion -->
+<label for="direcion">Direccion:</label><br>
+<input type="text" name="direccion" maxlength="255" required>
+<br/><br/>
+<!-- Telefono -->
+<label for="telefono">Telefono:</label><br>
+<input type="text" onkeypress="return valida(event) "name="telefono" maxlength="15" required>
+<br/><br/>
+<!-- Email -->
+<label for="correo">Email:</label><br>
+<input type="email" name="correo" maxlength="35" required>
+<br/><br/>
+<!--Nombre_usuario-->
+<label for="nombre">Nombre Usuario:</label><br>
+<input type="text" name="nombre_usuario" maxlength="255"  required>
+<br/><br/>
+<!--Password-->
+<label for="pass">Password:</label><br>
+<input type="password" name="password" maxlength="8" required>
+ <br/><br/>
+ <input type="submit" name="submit" class="btn btn-primary" value="Registrarme">
+ <input type="reset" name="clear" class="btn btn-primary" value="Borrar">
+ 
+   
+ </form>
+ 
+ </body>
 </html>
+© 2020 GitHub, Inc.
